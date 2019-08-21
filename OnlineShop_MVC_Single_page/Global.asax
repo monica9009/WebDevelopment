5761699e217100a64a9572569aca6dd8d9a6e979
monica9009 <monica.d9009@gmail.com>
2019-08-21 13:48:38 +0430

    Add files via upload

---
+++ OnlineShop_MVC_Single_page/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlineShop_MVC_Single_page.MvcApplication" Language="C#" %>
---
+++ OnlineShop_MVC_Single_page/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OnlineShop_MVC_Single_page
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
